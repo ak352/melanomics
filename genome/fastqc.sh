1b11d038b85875e0f6ee27666f99cf0cfec89ced
Abhimanyu Krishna <ak@uni.fake>
2014-04-04 16:21:03 +0200

    Added decompress FASTQ

--- genome/fastqc.sh
+++ genome/fastqc.sh
@@ -8,5 +8,22 @@ fqc()
     time $fastqc -f fastq -o $2 $1 
 }
 
+fqc_sample()
+{
+    sample=$1
+    folder=/work/projects/melanomics/analysis/genome/
+    for k in ${folder}/$sample/*.fastq
+    do
+	echo $k
+    done
+
+}
+
 #Test
-fqc test/input/test.fastq test/output/fastqc/
+#fqc test/input/test.fastq test/output/fastqc/
+
+for k in NHEM patient_2_NS patient_2_PM patient_4_NS patient_4_PM patient_5_NS patient_5_PM patient_6_NS patient_6_PM patient_7_NS patient_7_PM patient_8_NS patient_8_PM
+do
+    fqc_sample $k
+done
+
