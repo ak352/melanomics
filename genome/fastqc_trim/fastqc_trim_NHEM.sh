bea8c62fd1ba3fca0ea078ae4ee1044d021e4912
Abhimanyu Krishna <ak@uni.fake>
2014-05-20 10:36:24 +0200

    Added fastqc post read trimming

---
+++ genome/fastqc_trim/fastqc_trim_NHEM.sh
@@ -0,0 +1,64 @@
+#!/bin/bash --login
+
+fastqc=/work/projects/melanomics/tools/fastqc/FastQC/fastqc
+
+#sample=patient_2
+sample=$1
+
+run_fastqc()
+{
+    date
+    echo "STATUS: Run FastQC ..."
+    input=${files}
+    tmp_output=${TMP_DIR}
+    output=${OUT_DIR}
+    threads=14
+
+    CMD="time ${fastqc} -t ${threads} -o ${tmp_output} ${input}"
+    echo $CMD
+    eval $CMD
+    echo "FastQC done."
+    echo
+
+    echo "STATUS: Copying FastQC results from ${tmp_output} to ${output} ..."
+    # Create checksum, i.e. file fingerprint
+    md5sum ${tmp_output}/*
+    # Check filesize etc.
+    ls -l ${tmp_output}
+    cp -av ${tmp_output}/* ${output}
+    RETSTAT=$?
+    if [ ${RETSTAT} -eq 0 ]; then
+	echo "done."
+    else
+	echo "ERROR: Copy UNsuccessful"
+    fi
+    md5sum ${output}/*
+    ls -l ${output}
+    echo "Copying done."
+    date
+}
+
+
+make_directory()
+{
+    date
+    echo "STATUS: Create directory ..."
+    files=/work/projects/melanomics/analysis/genome/${sample}/trim/*_trimmed_*
+    TMP_DIR=${SCRATCH}/fastqc_trim/${sample}
+    CMD="mkdir -pv ${TMP_DIR}"
+    echo $CMD
+    eval $CMD
+    OUT_DIR=/work/projects/melanomics/analysis/genome/${sample}/fastqc_trim
+    CMD="mkdir -pv ${OUT_DIR}"
+    echo $CMD
+    eval $CMD
+    echo "STATUS: Directory created."
+    date
+}
+
+
+make_directory
+run_fastqc
+
+echo "DONE."
+
---
+++ genome/fastqc_trim/fastqc_trim_patients.sh
@@ -0,0 +1,71 @@
+#!/bin/bash --login
+
+fastqc=/work/projects/melanomics/tools/fastqc/FastQC/fastqc
+
+#sample=patient_2
+sample=$1
+
+run_fastqc()
+{
+    date
+    echo "STATUS: Run FastQC ..."
+    input=${files}
+    tmp_output=${TMP_DIR}
+    output=${OUT_DIR}
+    threads=12
+
+    CMD="time ${fastqc} -t ${threads} -o ${tmp_output} ${input}"
+    echo $CMD
+    eval $CMD
+    echo "FastQC done."
+    echo
+
+    echo "STATUS: Copying FastQC results from ${tmp_output} to ${output} ..."
+    # Create checksum, i.e. file fingerprint
+    md5sum ${tmp_output}/*
+    # Check filesize etc.
+    ls -l ${tmp_output}
+    cp -av ${tmp_output}/* ${output}
+    RETSTAT=$?
+    if [ ${RETSTAT} -eq 0 ]; then
+	echo "done."
+    else
+	echo "ERROR: Copy UNsuccessful"
+    fi
+    md5sum ${output}/*
+    ls -l ${output}
+    echo "STATUS: Copying done."    
+    date
+}
+
+make_directories()
+{
+  date
+  echo "STATUS: Create directories ..."
+  files=/work/projects/melanomics/analysis/genome/${sample}/${condition}/trim/*_trimmed_*
+  TMP_DIR=${SCRATCH}/fastqc_trim/${sample}_${condition}
+  CMD="mkdir -pv ${TMP_DIR}"
+  echo $CMD
+  eval $CMD
+  OUT_DIR=/work/projects/melanomics/analysis/genome/${sample}/${condition}/fastqc_trim
+  CMD="mkdir -pv ${OUT_DIR}"
+  echo $CMD
+  eval $CMD
+  echo "STATUS: Directories created."
+  date
+}
+
+# Normal skin
+condition="NS"
+
+make_directories
+run_fastqc
+
+# Primary melanoma
+condition="PM"
+
+make_directories
+run_fastqc
+
+echo "DONE."
+
