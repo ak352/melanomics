9c04721a3d30940700f31ab530e5e7e9d2f687ab
Abhimanyu Krishna <ak@uni.fake>
2015-08-06 12:00:12 +0200

    Add convert SV to CG testvariants format

---
+++ genome/sv/combine/SV2TestVariants/del_dup_inv/lumpy2testvariants.py
@@ -0,0 +1,87 @@
+import sys
+#from pylab import *
+
+def lumpy2test(sample):
+    infile="/work/projects/melanomics/analysis/genome/lumpy_genome/trim/%s.tumor_v_normal.pesr.bedpe" % sample
+    annotations = [("DELETION", "DEL"), ("DUPLICATION", "DUP"), ("INTERCHROM", "TRA"), ("INVERSION", "INV")]
+    logfile="/work/projects/melanomics/analysis/genome/lumpy_genome/trim/%s.deldupinv.testvariants.log" % sample
+    log = open(logfile, "w")
+
+    count = {}
+    somatic = {}
+    for anno in annotations:
+        out="/work/projects/melanomics/analysis/genome/lumpy_genome/trim/%s.%s.testvariants.1" % (sample, anno[1])
+        outfile=open(out, "w")
+        outfile.write(">chrom\tbegin\tend\n")
+
+        count[anno] = 0
+        somatic[anno] = 0
+        for line in open(infile):
+            line = line[:-1].split("\t")
+            svtype = line[10]
+            if svtype == "TYPE:%s" % anno[0]:
+                ids = line[11]
+                samples = [int(x.split(",")[0].lstrip("IDS:"))  for x in ids.split(";")]
+                loci = line[14]
+                if 2 in samples and 1 not in samples:
+                    # print "SOMATIC:", svtype, ids, loci
+                    loci = [x.split(":") for x in loci.split(";")]
+                    loci[0]  = loci[0][1:]
+                    for x in loci:
+                        x.append(x[1].split("-")[1])
+                        x[1] = x[1].split("-")[0]
+                    outfile.write("\t".join([loci[0][0], loci[0][1], loci[1][2]]) + "\n")
+                    somatic[anno] += 1
+                count[anno] += 1
+        sys.stderr.write("Output written to %s\n" % out)
+    log.write("sample\tSV type\tsomatic\ttotal\n")
+    for anno in annotations:
+        log.write("%s\t%s\t%d\t%d\n" % (sample, anno[0], somatic[anno], count[anno]))
+
+def plotter(sample, svtypes):
+    logfile="/work/projects/melanomics/analysis/genome/lumpy_genome/trim/%s.deldupinv.testvariants.log" % sample
+    with open(logfile) as f:
+        next(f)
+        for line in f:
+            line = line[:-1].split("\t")
+            sid = line[0]
+            svtype = line[1]
+            if svtype not in svtypes:
+                svtypes[svtype] = {}
+                svtypes[svtype]["somatic"] = []
+                svtypes[svtype]["all"] = [] 
+            somatic = int(line[2])
+            total = int(line[3])
+            svtypes[svtype]["somatic"].append(somatic)
+            svtypes[svtype]["all"].append(total)
+    return svtypes
+
+def plotter_all(svtypes):
+    figure(figsize=(22,20))
+    types = ["DELETION", "DUPLICATION", "INVERSION", "INTERCHROM"]
+    for i,svtype in enumerate(types):
+        subplot(2,2,i+1)
+        bar(arange(len(svtypes[svtype]["all"])), asarray(svtypes[svtype]["all"]) - asarray(svtypes[svtype]["somatic"]), label="non-somatic", color="y")
+        bar(arange(len(svtypes[svtype]["all"])), asarray(svtypes[svtype]["somatic"]), bottom=asarray(svtypes[svtype]["all"]) - asarray(svtypes[svtype]["somatic"]), label="somatic", color="g")
+        title(svtype)
+        ylabel("Number of structural variants")
+        #print svtype, somatic, total
+        
+        xticks(arange(len(svtypes[svtype]["all"]))+0.5, [str("patient_%d" % p) for p in [2,4,5,6,7,8]])
+    legend()
+    outfile="/work/projects/melanomics/analysis/genome/lumpy_genome/trim/graph/somatic.pdf"
+    savefig(outfile, bbox_inches="tight")
+    sys.stderr.write("Plot saved as %s\n" % outfile)
+    #show()
+
+if __name__ == "__main__":
+    snums=[2,4,5,6,7,8]
+    svtypes = {}
+    for snum in snums:
+        sample="patient_%d" % snum
+        lumpy2test(sample)
+        #svtypes = plotter(sample, svtypes)
+    #plotter_all(svtypes)
+
+
+
---
+++ genome/sv/combine/SV2TestVariants/translocations/delly2testvariants.py
@@ -0,0 +1,115 @@
+from pylab import *
+import os
+import datetime
+
+#Creates a dictionary of fields pointing to column numbers, makes the code more readable
+def ParseFields(line):
+    fields = {}
+    var = line.rstrip("\n").lstrip("#").lstrip(">").split("\t")
+    for x in range(0, len(var)):
+        fields[var[x]] = x
+    return fields
+
+def lg(num):
+    return log(num)/log(10)
+
+def is_somatic(g1, g2):
+    g1 = g1.split(":")[0]
+    g2 = g2.split(":")[0]
+    is_somatic=False
+    if g1 not in ["./.", ".", "0/0"]:
+        return is_somatic
+    if g2 in ["./.", ".", "0/0"]:
+        return is_somatic
+
+    # If g1 has no ALT allele and g2 has an ALT allele, 
+    # return True, else False
+    return True
+    
+
+def get_delly_deletions(infile, outfile, logfile):
+    out = open(outfile, "w")
+    out.write("chrom1\tchrom2\tpos1\tpos2\n")
+
+    count_all = 0
+    count_somatic = 0
+
+    with open(infile) as f:
+        line = "##"
+        while line.startswith("##"):
+            line = next(f)
+        header = line
+        var = ParseFields(header)
+
+        svlens = []
+        svlens_all_qual = []
+        for line in f:
+            line = line[:-1].split("\t")
+            info = line[var["INFO"]].split(";")
+            fields = {}
+            for item in info:
+                item = item.split("=")
+                #sys.stderr.write("Item %s found \n" % item)
+                if len(item) > 1:
+                    fields[item[0]] = item[1]
+            # print line[var["CHROM"]], line[var["POS"]], fields["CHR2"], fields["END"], fields["SVLEN"]
+            chrom1 = line[var["CHROM"]]
+            chrom2 = fields["CHR2"]
+            pos1 = int(line[var["POS"]])
+            pos2 = int(fields["END"])
+            
+            svlen = int(fields["SVLEN"])
+            if line[var["FILTER"]] != "LowQual":
+                # Check if somatic
+                if is_somatic(line[9], line[10]):
+                    svlens.append(svlen)
+                    outline = [chrom1, chrom2, str(pos1), str(pos2)]
+                    out.write("\t".join(outline)+"\n")
+                    count_somatic += 1
+                count_all += 1
+
+            svlens_all_qual.append(svlen)
+
+    logfile.write("\t%d\t%d\n" % (count_somatic, count_all))
+    y, nbins = histogram(svlens, 100)
+    y2, nbins2 = histogram(svlens_all_qual, 100)
+    nbins = (nbins[1:]+nbins[:-1])/2
+    nbins2 = (nbins2[1:]+nbins2[:-1])/2
+    return (nbins, y), (nbins2, y2)
+
+
+if __name__=="__main__":
+    samples = [2,4,5,6,7,8]
+    logfile = "/work/projects/melanomics/analysis/genome/sv/delly/delly.testvariants.TRA.log"
+    sys.stderr.write("Logfile: %s\n" % logfile)
+    log = open(logfile, "a")
+    log.write("[DATE,TIME] " + str(datetime.datetime.now()) + "\n")
+    log.write("Converting translocations to testvariants format...\n")
+    log.write("patient\tSV type\tsomatic\tall\n")
+
+    svtypes = ["TRA"]
+    svtype_full = ["INTERCHROM"]
+
+    for sample in samples:
+        for i,svtype in enumerate(svtypes):
+            infile="/work/projects/melanomics/analysis/genome/sv/delly/patient_%d/final/patient_%d_PM.%s.vcf" % (sample, sample, svtype)
+            if not os.path.isfile(infile):
+                continue
+            outfile="/work/projects/melanomics/analysis/genome/sv/delly/patient_%d/final/patient_%d_PM.%s.testvariants" % (sample, sample, svtype)
+            log.write("patient_%d\t%s" % (sample, svtype_full[i]))
+            highq, alldel = get_delly_deletions(infile, outfile, log)    
+            sys.stderr.write("Output written to %s\n" % outfile)
+
+    sys.stderr.write("Log output file: %s\n" % logfile)
+
+
+
+    # subplot(2,3,i+1)
+    # plot(lg(highq[0]), lg(highq[1]), label="DELLY_HQ", lw=3)
+    # plot(lg(alldel[0]), lg(alldel[1]), label="DELLY", lw=3)
+    # sample = 2
+    # title("patient_%d" % sample)
+    # xlabel("log(Size of deletions (bp))")
+    # ylabel("log(Number of variants)")
+    # legend()
+    # show()
